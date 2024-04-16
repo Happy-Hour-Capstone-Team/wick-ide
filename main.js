545607769e1ade409d02f6eb3a3e3376b9c6b3d6
evelincerros <evelincerrospatricio@gmail.com>
2024-04-16 17:26:31 -0500

    ..

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 function runCode(){ 
     const userCode = document.getElementById('code-editor').value;
@@ -29,6 +30,34 @@ function fetchCppHeaders() {
     const fetchPromises = headerFiles.map(file => fetch(file).then(response => response.text()));
 
     return Promise.all(fetchPromises);
+=======
+function initializeIDE() {
+  document.getElementById("runButton").addEventListener("click", runningCode);
+}
+
+async function runningCode() {
+  var userCode = document.getElementById("code-editor").value;
+
+  try {
+    const response = await fetch("/run", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: userCode }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to execute code");
+    }
+
+    const result = await response.json();
+
+    document.getElementById("output").innerText = result.output;
+  } catch (error) {
+    console.error("Error executing code:", error);
+  }
+>>>>>>> Stashed changes
 }
 
 function executeCode(code) {
